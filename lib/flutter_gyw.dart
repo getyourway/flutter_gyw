339b2db08fd38693b817defa77a23d033028f184
Antoine Malherbe <a.malherbe@getyourway.be>
2022-10-05 18:05:32 +0200

    Drawings + correct screen parameters

--- lib/flutter_gyw.dart
+++ lib/flutter_gyw.dart
@@ -3,4 +3,6 @@ library flutter_gyw;
 export 'src/bt_device.dart' show BTDevice;
 export 'src/bt_manager.dart' show BTManager;
 export 'src/exceptions.dart' show GYWException, GYWStatusException;
-export 'src/parameters.dart' show GYWScreenParameters;
+export 'src/screen.dart' show GYWScreenParameters;
+export 'src/fonts.dart' show GYWFont;
+export 'src/drawings.dart' show Drawing, TextDrawing, WhiteScreen;
--- lib/src/parameters.dart
+++
@@ -1,8 +0,0 @@
-class GYWScreenParameters {
-  static int width = 800;
-  static int height = 480;
-  static int simpleTextHeight = 20; // TODO
-  static int titleTextHeight = 40; // TODO
-  static int simpleTextWidthPerCharacter = 10; // TODO
-  static int titleTextWidthPerCharacter = 10; // TODO
-}
